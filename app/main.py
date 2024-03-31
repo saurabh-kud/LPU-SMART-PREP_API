c13b8aa0c54ec2850fa3ea3a661e7f66308c62bd
saurabh-kud <saurabh322001raj@gmail.com>
2024-03-31 17:29:08 +0530

    minor fixed

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import pickle
 import pandas as pd
-
+from pydantic import BaseModel
 app = FastAPI()
 
 pickle_in = open("final_model.pkl","rb")
-print(pickle_in)
+
 classifier=pickle.load(pickle_in)
 
 
@@ -18,33 +18,28 @@ async def health_route(req: Request):
     """
     return JSONResponse(
         {
-            "message": "hello world"
-        }
-    )
-
-@app.get("/health")
-async def health_route(req: Request):
-    """
-    Health Route : Returns App details.
-
-    """
-    return JSONResponse(
-        {
-            "message": "hello world health"
+            "message": "welcome to prediction module of lpu smart prep"
         }
     )
 
 
-@app.get("/predict")
-def health_route(req: Request):
+
+class PredictionRequest(BaseModel):
+    data: list
+
+
+@app.post("/predict")
+async def predict_model(data: PredictionRequest):
     """
-    Health Route : Returns App details.
+    for prediction test result
 
     """
-    dummy = pd.DataFrame([[50, 60, 70, 80, 90]], columns=['FirstScore', 'SecondScore', 'ThirdScore', 'FourthScore', 'FifthScore'])
+    print(data.data)
+    
+    dummy = pd.DataFrame([data.data], columns=['FirstScore', 'SecondScore', 'ThirdScore', 'FourthScore', 'FifthScore'])
 
     prediction = classifier.predict(dummy)
-    print(prediction)
+    
     return JSONResponse(
         {
             "message": prediction[0]
